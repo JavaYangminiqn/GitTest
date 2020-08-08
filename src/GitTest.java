e80cea593366242a865f43e869287f80eb77d21b
JavaYangminqin <ymq19980521@163.com>
2020-08-08 10:59:57 +0800

    ymq-版本一--创建main方法 ymq-版本一--创建run方法

--- src/GitTest.java
+++ src/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
         System.out.println("hello git");
     }
     public void run(){
-        System.out.println("run");
+        System.out.println("run3123123");
     }
 }
